2cf9d1314294ad7caad135102bb4cdf5b386408a
Paul Ongkiko <paul.ongkiko@slu.edu>
2023-04-26 16:53:08 -0500

    card and cardBoard

--- app/src/main/java/MemoryGAME/Model/CardBoard.java
+++ app/src/main/java/MemoryGAME/Model/CardBoard.java
@@ -1,18 +1,49 @@
 package MemoryGame.model;
 
 import java.util.ArrayList;
+import java.util.Random;
 //import MemoryGame.GameObserver;
 
 public class CardBoard
 {
 	private Card[][] cards;
+    private int[][] cardValues = {{1, 1, 2, 2}, {3, 3, 4, 4}, {5, 5, 6, 6}, {7, 7, 8, 8}};
 	//private ArrayList<GameObserver> observers = new ArrayList<GameObserver>();
 
 	public CardBoard()
 	{
-		this.cards = new Card[4][4];
+        this.cards = new Card[4][4];
+        this.randomizeCards();
+
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                cards[i][j] = new Card(cardValues[i][j]);
+            }
+        }
 	}
 	
+    public void randomizeCards()
+    {
+        // Create a new Random object
+        Random rand = new Random();
+
+        // Iterate through each element in the array
+        for (int i = 0; i < cardValues.length; i++) {
+            for (int j = 0; j < cardValues[i].length; j++) {
+                // Generate random indices for swapping
+                int iRand = rand.nextInt(cardValues.length);
+                int jRand = rand.nextInt(cardValues[i].length);
+
+                // Swap the current element with the randomly selected element
+                int temp = cardValues[i][j];
+                cardValues[i][j] = cardValues[iRand][jRand];
+                cardValues[iRand][jRand] = temp;
+            }
+        }
+    }
+
 	public boolean checkMatch(Card card1, Card card2)
 	{
 		if(card1.getValue() == card2.getValue())
--- app/src/main/java/MemoryGAME/controller/MemoryGameController.java
+++ app/src/main/java/MemoryGAME/controller/MemoryGameController.java
@@ -2,11 +2,14 @@ package MemoryGame.controller;
 
 import MemoryGame.MemoryGameControllerInterface;
 import MemoryGame.view.MemoryGameGUI;
+import MemoryGame.model.CardBoard;
 
 public class MemoryGameController implements MemoryGameControllerInterface {
     
     public MemoryGameController() {
         new MemoryGameGUI();
+        CardBoard board = new CardBoard();
+        board.printCards();
     }
 
 }
