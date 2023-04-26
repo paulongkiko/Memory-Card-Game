82fedc96fc5f81be876950be5dad9d4fd8d49eff
Paul Ongkiko <paul.ongkiko@slu.edu>
2023-04-26 16:27:03 -0500

    changed model classes

--- app/src/main/java/MemoryGAME/Model/CardBoard.java
+++ app/src/main/java/MemoryGAME/Model/CardBoard.java
@@ -1,6 +1,27 @@
 package MemoryGame.model;
 
-public class CardBoard {
+import java.util.ArrayList;
+import MemoryGame.GameObserver;
 
-    
+public class CardBoard
+{
+	private Card[][] cards;
+	private ArrayList<GameObserver> observers = new ArrayList<GameObserver>();
+
+	public CardBoard()
+	{
+		this.cards = new Card[4][4];
+	}
+	
+	public boolean checkMatch(Card card1, Card card2)
+	{
+		if(card1.getValue() == card2.getValue())
+		{
+			return true;
+		} else {
+			card1.unflip();
+			card2.unflip();
+			return false;
+		}
+	}
 }
